6ecf59d80609e2277dbfe25650abacdfc2197500
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2024-08-09 11:01:39 +0200

    Add missing client method `refreshNewspapers` for works

--- metasvc-client/src/main/java/de/digitalcollections/cudami/client/identifiable/entity/work/CudamiWorksClient.java
+++ metasvc-client/src/main/java/de/digitalcollections/cudami/client/identifiable/entity/work/CudamiWorksClient.java
@@ -1,5 +1,7 @@
 package de.digitalcollections.cudami.client.identifiable.entity.work;
 
+import static de.digitalcollections.cudami.client.CudamiRestClient.API_VERSION_PREFIX;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.digitalcollections.cudami.client.identifiable.entity.CudamiEntitiesClient;
 import de.digitalcollections.model.exception.TechnicalException;
@@ -8,6 +10,7 @@ import de.digitalcollections.model.identifiable.entity.manifestation.Manifestati
 import de.digitalcollections.model.identifiable.entity.work.Work;
 import de.digitalcollections.model.list.paging.PageRequest;
 import de.digitalcollections.model.list.paging.PageResponse;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.net.http.HttpClient;
 import java.util.List;
 import java.util.Locale;
@@ -15,6 +18,9 @@ import java.util.Set;
 import java.util.UUID;
 
 public class CudamiWorksClient extends CudamiEntitiesClient<Work> {
+
+  @SuppressFBWarnings(value = "SS_SHOULD_BE_STATIC", justification = "non-static is fine, though")
+  private final String digiPressBaseEndpoint = API_VERSION_PREFIX + "/digipress";
 
   public CudamiWorksClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
     super(http, serverUrl, Work.class, mapper, API_VERSION_PREFIX + "/works");
@@ -42,5 +48,9 @@ public class CudamiWorksClient extends CudamiEntitiesClient<Work> {
   public List<Locale> getLanguagesOfManifestations(UUID uuid) throws TechnicalException {
     return doGetRequestForObjectList(
         String.format("%s/%s/manifestations/languages", baseEndpoint, uuid), Locale.class);
+  }
+
+  public void refreshNewspapers() throws TechnicalException {
+    doPutRequestForObject(digiPressBaseEndpoint + "/refresh", null);
   }
 }
