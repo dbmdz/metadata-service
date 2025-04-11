b714ef7a1c6efcaf00c031c23a35fe45ca8c1d0e
David Förster <david.foerster@bsb-muenchen.de>
2025-04-11 12:25:03 +0200

    Merge backend-file into webapp

--- metasvc-server/backend-file/src/main/java/io/github/dbmdz/metadata/server/config/SpringConfigBackendFile.java
+++
@@ -1,9 +0,0 @@
-package io.github.dbmdz.metadata.server.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-/** File repository configuration. */
-@Configuration
-@ComponentScan(basePackages = {"de.digitalcollections.cudami.server.backend.impl.file"})
-public class SpringConfigBackendFile {}
